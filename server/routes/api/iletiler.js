b6d9750fd80dea988aa5e510719272d8b3580c88
Emir Mertoglu <emirxmertoglu@outlook.com>
2019-12-04 20:37:37 +0300

    mongodb baglantisi yapildi

--- server/routes/api/iletiler.js
+++ server/routes/api/iletiler.js
@@ -4,12 +4,32 @@ const mongodb = require('mongodb');
 const router = express.Router();
 
 // iletileri GET metodu ile al
-router.get('/', (req, res) => {
-    res.send('Merhaba..');
-});
+// router.get('/', (req, res) => {
+//     res.send('Merhaba..');
+// });
+router.get('/', async (req, res) => {
+    // dondurulen koleksiyonlari iletiler degiskenine yukle
+    const iletiler = await loadIletilerCollection();
+    // db icindeki iletiler dizisini dondur ve istemciye cevap olarak gonder
+    // find yontemindeki bos kume parantezleri herhangi bir kriter verilmediginden
+    // butun iletileri getirir
+    res.send(await iletiler.find({}).toArray());
+})
 
 // ileti ekle
 
 // ileti sil
+
+async function loadIletilerCollection(){
+    const client = await mongodb.MongoClient.connect
+    (
+        'mongodb+srv://emirmertoglu:Emir3647*@cluster0-kbrgd.mongodb.net/test?retryWrites=true&w=majority',
+        {
+            useNewUrlParser : true,
+            useUnifiedTopology : true
+        }
+    );
+    return client.db('emirProje').collection('iletiler');
+}
 
 module.exports = router;
