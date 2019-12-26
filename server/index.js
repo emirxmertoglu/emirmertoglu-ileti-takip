ad446bb4a9e2420a65cb21af848bac53bca8db4e
Emir Mertoglu <emirxmertoglu@outlook.com>
2019-12-26 15:47:21 +0300

    son commit

--- server/index.js
+++ server/index.js
@@ -13,6 +13,11 @@ app.use(cors());
 const iletiler = require('./routes/api/iletiler');
 app.use('/api/iletiler', iletiler);
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(__dirname + '/public/'));
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
 // server localde 5000, remoteda herokunun belirledigi portta calisir
 const port = process.env.PORT || 3647;
 
